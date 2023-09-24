63d8e97be1156b4b7e8b825debf048f3c1309d45
Kenneth Carnes <kennethmcarnes@gmail.com>
2023-09-24 18:03:15 -0500

    testing

--- scripts/Set-CosmosDBRoleDelegation.ps1
+++ scripts/Set-CosmosDBRoleDelegation.ps1
@@ -9,63 +9,85 @@ param(
 
     [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
-    [string] $servicePrincipalClientId
+    [string] $servicePrincipalClientId,
+
+    # New: Add a debug switch to control verbose logging.
+    [switch] $debug = $false
 )
 
-try {
-    # 1. Get the Cosmos DB resource ID
-    $subscriptionId = (Get-AzContext).Subscription.Id
-    $cosmosDbResourceId = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.DocumentDB/databaseAccounts/$cosmosDbAccountName"
+function ExitWithError {
+    param (
+        [string] $message,
+        [int] $exitCode
+    )
+    Write-Error $message
+    if ($_.Exception.InnerException) {
+        Write-Error "Inner Exception: $($_.Exception.InnerException)"
+    }
+    Write-Error "Full Exception: $_"
+    Write-Error "StackTrace: $($_.Exception.StackTrace)"
+    exit $exitCode
+}
 
-    # Validate Cosmos DB Account exists
-    $cosmosDbAccount = Get-AzResource -ResourceId $cosmosDbResourceId
-    if (-not $cosmosDbAccount) {
-        throw "Cosmos DB Account '$cosmosDbAccountName' does not exist in resource group '$resourceGroupName'."
+try {
+    if ($debug) {
+        $VerbosePreference = 'Continue'
     }
 
-    # 2. Update the custom role with the Cosmos DB scope
+    Write-Verbose "Resource Group Name: $resourceGroupName"
+    Write-Verbose "Cosmos DB Account Name: $cosmosDbAccountName"
+    Write-Verbose "Service Principal Client ID: $servicePrincipalClientId"
+
+    Write-Host "Retrieving subscription ID and constructing Cosmos DB resource ID..."
+    $subscriptionId = (Get-AzContext -Verbose:$debug).Subscription.Id
+    if (-not $subscriptionId) { ExitWithError "Failed to retrieve Subscription ID" 2 }
+
+    $cosmosDbResourceId = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.DocumentDB/databaseAccounts/$cosmosDbAccountName"
+    Write-Host "Cosmos DB Resource ID: $cosmosDbResourceId"
+
+    Write-Host "Validating existence of Cosmos DB Account..."
+    $cosmosDbAccount = Get-AzResource -ResourceId $cosmosDbResourceId -Verbose:$debug
+    if (-not $cosmosDbAccount) { ExitWithError "Cosmos DB Account '$cosmosDbAccountName' does not exist in resource group '$resourceGroupName'." 3 }
+
+    Write-Host "Validated: Cosmos DB Account exists."
+
+    Write-Host "Setting up custom role properties..."
     $customRoleProperties = @{
         Name             = "Custom Role Assignment Delegate"
         Description      = "Allows for role assignment within a specified scope."
         Actions          = @("Microsoft.Authorization/roleAssignments/write", "Microsoft.Authorization/roleAssignments/delete", "Microsoft.Authorization/roleAssignments/read")
         AssignableScopes = @($cosmosDbResourceId)
     }
+    Write-Verbose "Custom role properties: $($customRoleProperties | Out-String)"
 
-# Check and create the custom role if it does not exist
-$existingRole = Get-AzRoleDefinition -Name "Custom Role Assignment Delegate" -ErrorAction SilentlyContinue
+    Write-Host "Checking if custom role exists..."
+    $existingRole = Get-AzRoleDefinition -Name "Custom Role Assignment Delegate" -Verbose:$debug -ErrorAction SilentlyContinue
+    if ($null -eq $existingRole) {
+        Write-Host "Role 'Custom Role Assignment Delegate' does not exist. Creating it now."
+        New-AzRoleDefinition @customRoleProperties -Verbose:$debug
+    } else {
+        Write-Host "Role exists. Updating its properties..."
+        $existingRole.Actions = $customRoleProperties.Actions
+        $existingRole.AssignableScopes = $customRoleProperties.AssignableScopes
+        $existingRole | Set-AzRoleDefinition -Verbose:$debug
+    }
 
-if ($null -eq $existingRole) {
-    Write-Host "Role 'Custom Role Assignment Delegate' does not exist. Creating it now."
-    New-AzRoleDefinition @customRoleProperties
-} else {
-    $existingRole.Actions = $customRoleProperties.Actions
-    $existingRole.AssignableScopes = $customRoleProperties.AssignableScopes
-    $existingRole | Set-AzRoleDefinition
-}
+    Write-Host "Retrieving Object ID of the Service Principal using its Client ID..."
+    $sp = Get-AzADServicePrincipal -ApplicationId $servicePrincipalClientId -Verbose:$debug
+    if (-not $sp) { ExitWithError "Failed to retrieve Service Principal by its Application ID: $servicePrincipalClientId" 4 }
 
+    $spObjectId = $sp.Id
+    Write-Verbose "Service Principal Object ID: $spObjectId"
 
-# Get the ObjectId of the Service Principal using the passed ClientId
-$sp = Get-AzADServicePrincipal -ApplicationId $servicePrincipalClientId
-$spObjectId = $sp.Id
-
-# Check if the role assignment already exists for the given Service Principal
-$existingAssignment = Get-AzRoleAssignment -ObjectId $spObjectId -RoleDefinitionName "Custom Role Assignment Delegate" -Scope $cosmosDbResourceId -ErrorAction SilentlyContinue
-
+    Write-Host "Checking if role assignment exists for Service Principal..."
+    $existingAssignment = Get-AzRoleAssignment -ObjectId $spObjectId -RoleDefinitionName "Custom Role Assignment Delegate" -Scope $cosmosDbResourceId -Verbose:$debug -ErrorAction SilentlyContinue
     if ($null -eq $existingAssignment) {
-        New-AzRoleAssignment `
-            -ObjectId $spObjectId `
-            -RoleDefinitionName "Custom Role Assignment Delegate" `
-            -Scope $cosmosDbResourceId
-        Write-Host "Role 'Custom Role Assignment Delegate' assigned to the Service Principal at Cosmos DB scope."
+        Write-Host "No existing role assignment found. Assigning now..."
+        New-AzRoleAssignment -ObjectId $spObjectId -RoleDefinitionName "Custom Role Assignment Delegate" -Scope $cosmosDbResourceId -Verbose:$debug
     } else {
         Write-Host "Role 'Custom Role Assignment Delegate' is already assigned to the Service Principal at Cosmos DB scope. No action needed."
     }
 
-    }
-    catch {
-        Write-Error "Caught an exception: $($_.Exception.Message)"
-        Write-Error "Full Exception: $_"
-        Write-Error "StackTrace: $($_.Exception.StackTrace)"
-        exit 1
-    }
-    
+} catch {
+    ExitWithError "Caught an exception: $($_.Exception.Message)" 1
+}
