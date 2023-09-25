82e0d84d6a1c5f3b84fd8e511c43c28ff71ecc0b
Kenneth Carnes <kennethmcarnes@gmail.com>
2023-09-24 20:36:26 -0500

    testing

--- scripts/Set-CosmosDBRoleDelegation.ps1
+++ scripts/Set-CosmosDBRoleDelegation.ps1
@@ -27,7 +27,6 @@ function ExitWithError {
 }
 
 try {
-
     Write-Host "Retrieving subscription ID and constructing Cosmos DB resource ID..."
     $subscriptionId = (Get-AzContext).Subscription.Id
     if (-not $subscriptionId) { ExitWithError "Failed to retrieve Subscription ID" 2 }
@@ -38,44 +37,24 @@ try {
     Write-Host "Validating existence of Cosmos DB Account..."
     $cosmosDbAccount = Get-AzResource -ResourceId $cosmosDbResourceId
     if (-not $cosmosDbAccount) { ExitWithError "Cosmos DB Account '$cosmosDbAccountName' does not exist in resource group '$resourceGroupName'." 3 }
-
     Write-Host "Validated: Cosmos DB Account exists."
 
-    Write-Host "Setting up custom role properties..."
-    $customRoleProperties = @{
-        Name             = "Custom Role Assignment Delegate"
-        Description      = "Allows for role assignment within a specified scope."
-        Actions          = @("Microsoft.Authorization/roleAssignments/write", "Microsoft.Authorization/roleAssignments/delete", "Microsoft.Authorization/roleAssignments/read")
-        AssignableScopes = @($cosmosDbResourceId)
-    }
-
-    Write-Host "Checking if custom role exists..."
-    $existingRole = Get-AzRoleDefinition -Name "Custom Role Assignment Delegate" -ErrorAction SilentlyContinue
-    if ($null -eq $existingRole) {
-        Write-Host "Role 'Custom Role Assignment Delegate' does not exist. Creating it now."
-        New-AzRoleDefinition @customRoleProperties
-    } else {
-        Write-Host "Role exists. Updating its properties..."
-        $existingRole.Actions = $customRoleProperties.Actions
-        $existingRole.AssignableScopes = $customRoleProperties.AssignableScopes
-        $existingRole | Set-AzRoleDefinition
-    }
+    # Using built-in role 'User Access Administrator' instead of a custom role
+    $roleName = "User Access Administrator"
 
     Write-Host "Retrieving Object ID of the Service Principal using its Client ID..."
     $sp = Get-AzADServicePrincipal -ApplicationId $servicePrincipalClientId
     if (-not $sp) { ExitWithError "Failed to retrieve Service Principal by its Application ID: $servicePrincipalClientId" 4 }
-
     $spObjectId = $sp.Id
 
     Write-Host "Checking if role assignment exists for Service Principal..."
-    $existingAssignment = Get-AzRoleAssignment -ObjectId $spObjectId -RoleDefinitionName "Custom Role Assignment Delegate" -Scope $cosmosDbResourceId -ErrorAction SilentlyContinue
+    $existingAssignment = Get-AzRoleAssignment -ObjectId $spObjectId -RoleDefinitionName $roleName -Scope $cosmosDbResourceId -ErrorAction SilentlyContinue
     if ($null -eq $existingAssignment) {
         Write-Host "No existing role assignment found. Assigning now..."
-        New-AzRoleAssignment -ObjectId $spObjectId -RoleDefinitionName "Custom Role Assignment Delegate" -Scope $cosmosDbResourceId
+        New-AzRoleAssignment -ObjectId $spObjectId -RoleDefinitionName $roleName -Scope $cosmosDbResourceId
     } else {
-        Write-Host "Role 'Custom Role Assignment Delegate' is already assigned to the Service Principal at Cosmos DB scope. No action needed."
+        Write-Host "Role '$roleName' is already assigned to the Service Principal at Cosmos DB scope. No action needed."
     }
-
 } catch { 
-    ExitWithError "Caught an exception: $($_.Exception.Message)" 1
+    ExitWithError "Caught an exception: $($_)" 1
 }
