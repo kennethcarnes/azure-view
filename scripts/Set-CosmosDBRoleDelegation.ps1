fe2c9ead320c4f330d2edd4ff6ae5df95ae815e1
Kenneth Carnes <kennethmcarnes@gmail.com>
2023-09-24 20:20:42 -0500

    testing

--- scripts/Set-CosmosDBRoleDelegation.ps1
+++ scripts/Set-CosmosDBRoleDelegation.ps1
@@ -9,10 +9,7 @@ param(
 
     [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
-    [string] $servicePrincipalClientId,
-
-    # New: Add a debug switch to control verbose logging.
-    [switch] $debug
+    [string] $servicePrincipalClientId
 )
 
 function ExitWithError {
@@ -30,23 +27,16 @@ function ExitWithError {
 }
 
 try {
-    if ($debug) {
-        $VerbosePreference = 'Continue'
-    }
-
-    Write-Verbose "Resource Group Name: $resourceGroupName"
-    Write-Verbose "Cosmos DB Account Name: $cosmosDbAccountName"
-    Write-Verbose "Service Principal Client ID: $servicePrincipalClientId"
 
     Write-Host "Retrieving subscription ID and constructing Cosmos DB resource ID..."
-    $subscriptionId = (Get-AzContext -Verbose:$debug).Subscription.Id
+    $subscriptionId = (Get-AzContext).Subscription.Id
     if (-not $subscriptionId) { ExitWithError "Failed to retrieve Subscription ID" 2 }
 
     $cosmosDbResourceId = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.DocumentDB/databaseAccounts/$cosmosDbAccountName"
     Write-Host "Cosmos DB Resource ID: $cosmosDbResourceId"
 
     Write-Host "Validating existence of Cosmos DB Account..."
-    $cosmosDbAccount = Get-AzResource -ResourceId $cosmosDbResourceId -Verbose:$debug
+    $cosmosDbAccount = Get-AzResource -ResourceId $cosmosDbResourceId
     if (-not $cosmosDbAccount) { ExitWithError "Cosmos DB Account '$cosmosDbAccountName' does not exist in resource group '$resourceGroupName'." 3 }
 
     Write-Host "Validated: Cosmos DB Account exists."
@@ -58,32 +48,30 @@ try {
         Actions          = @("Microsoft.Authorization/roleAssignments/write", "Microsoft.Authorization/roleAssignments/delete", "Microsoft.Authorization/roleAssignments/read")
         AssignableScopes = @($cosmosDbResourceId)
     }
-    Write-Verbose "Custom role properties: $($customRoleProperties | Out-String)"
 
     Write-Host "Checking if custom role exists..."
-    $existingRole = Get-AzRoleDefinition -Name "Custom Role Assignment Delegate" -Verbose:$debug -ErrorAction SilentlyContinue
+    $existingRole = Get-AzRoleDefinition -Name "Custom Role Assignment Delegate" -ErrorAction SilentlyContinue
     if ($null -eq $existingRole) {
         Write-Host "Role 'Custom Role Assignment Delegate' does not exist. Creating it now."
-        New-AzRoleDefinition @customRoleProperties -Verbose:$debug
+        New-AzRoleDefinition @customRoleProperties
     } else {
         Write-Host "Role exists. Updating its properties..."
         $existingRole.Actions = $customRoleProperties.Actions
         $existingRole.AssignableScopes = $customRoleProperties.AssignableScopes
-        $existingRole | Set-AzRoleDefinition -Verbose:$debug
+        $existingRole | Set-AzRoleDefinition
     }
 
     Write-Host "Retrieving Object ID of the Service Principal using its Client ID..."
-    $sp = Get-AzADServicePrincipal -ApplicationId $servicePrincipalClientId -Verbose:$debug
+    $sp = Get-AzADServicePrincipal -ApplicationId $servicePrincipalClientId
     if (-not $sp) { ExitWithError "Failed to retrieve Service Principal by its Application ID: $servicePrincipalClientId" 4 }
 
     $spObjectId = $sp.Id
-    Write-Verbose "Service Principal Object ID: $spObjectId"
 
     Write-Host "Checking if role assignment exists for Service Principal..."
-    $existingAssignment = Get-AzRoleAssignment -ObjectId $spObjectId -RoleDefinitionName "Custom Role Assignment Delegate" -Scope $cosmosDbResourceId -Verbose:$debug -ErrorAction SilentlyContinue
+    $existingAssignment = Get-AzRoleAssignment -ObjectId $spObjectId -RoleDefinitionName "Custom Role Assignment Delegate" -Scope $cosmosDbResourceId -ErrorAction SilentlyContinue
     if ($null -eq $existingAssignment) {
         Write-Host "No existing role assignment found. Assigning now..."
-        New-AzRoleAssignment -ObjectId $spObjectId -RoleDefinitionName "Custom Role Assignment Delegate" -Scope $cosmosDbResourceId -Verbose:$debug
+        New-AzRoleAssignment -ObjectId $spObjectId -RoleDefinitionName "Custom Role Assignment Delegate" -Scope $cosmosDbResourceId
     } else {
         Write-Host "Role 'Custom Role Assignment Delegate' is already assigned to the Service Principal at Cosmos DB scope. No action needed."
     }
